0fe1112d0252ce9942497890f169e367747c37b1
GranollaBar <josnoble113@gmail.com>
2021-06-20 16:16:47 +0100

    AS Programming Project #16

--- AS Project Frames/CompetitionFrame/ResultsScreen.py
+++ AS Project Frames/CompetitionFrame/ResultsScreen.py
@@ -10,8 +10,6 @@ from functools import partial
 from tkcalendar import Calendar
 import datetime
 
-MemberCounter=0
-
 class ResultsContent:
 
 	def __init__(self, mainScreen):
@@ -116,6 +114,45 @@ class ResultsContent:
 				return False
 
 			label.config(fg="SpringGreen3")
+			return True
+
+
+		def validate_member_scores(value1, value2, fieldname1, fieldname2, label1, label2):
+			if (str(value1) == ''):
+				label1.config(fg="red")
+				messagebox.showinfo("Validation Error", "The Value For Field " + fieldname1 + " Can Not Be empty")
+				return False
+			if (value1.isnumeric() == False):
+				label1.config(fg="red")
+				messagebox.showinfo("Validation Error", "The Value For Field " + fieldname1 + " Must be an integer")
+				return False
+			if (int(value1) > 21):
+				label1.config(fg="red")
+				messagebox.showinfo("Validation Error", "The Value For Field " + fieldname1 + " Must be below 21")
+				return False
+			if (str(value2) == ''):
+				label1.config(fg="SpringGreen3")
+				label2.config(fg="red")
+				messagebox.showinfo("Validation Error", "The Value For Field " + fieldname2 + " Can Not Be empty")
+				return False
+			if (value2.isnumeric() == False):
+				label1.config(fg="SpringGreen3")
+				label2.config(fg="red")
+				messagebox.showinfo("Validation Error", "The Value For Field " + fieldname2 + " Must be an integer")
+				return False
+			if (int(value2) > 21):
+				label1.config(fg="SpringGreen3")
+				label2.config(fg="red")
+				messagebox.showinfo("Validation Error", "The Value For Field " + fieldname2 + " Must be below 21")
+				return False
+			if (int(value1) != 21 and int(value2) != 21):
+				label1.config(fg="red")
+				label2.config(fg="red")
+				messagebox.showinfo("Validation Error", "The Value For Field " + fieldname1 + " and " + fieldname2 + " Must have one score equal to 21")
+				return False
+
+			label1.config(fg="SpringGreen3")
+			label2.config(fg="SpringGreen3")
 			return True
 
 
@@ -285,55 +322,159 @@ class ResultsContent:
 			conn.close()
 
 
-		def completeCompetition():
+		def completeCompetition(label1, label2):
 			conn = sqlite3.connect('BadmintonClub.db')
 			c = conn.cursor()
 
+			if label1 == "" or label2 == "":
+				messagebox.showinfo("info", "No Group has been selected yet", icon="error")
+
+			else:
+
+				isValid = True
+				isValid = isValid and validate_group_selected(self.memberGroups.get(), "Member Group Selected", view_group_label)
+
+				if isValid:
+					member_group_selected = self.memberGroups.get()
+
+					c.execute("SELECT * From competition WHERE competitionID=?", (member_group_selected,))
+					items = c.fetchone()
+					if not items:
+						messagebox.showinfo("info", "There is no group that exists with that number", icon="error")
+
+					else:
+
+						memberUsername = items[0]
+						memberUsername2 = items[1]
+
+
+						completeFrame=Toplevel(bg="white")
+						completeFrame.geometry('200x200')
+
+						title_label =Label(completeFrame,text ="Input Competition Scores" , fg ='SpringGreen3',bg='white',font=('Tahoma',11,'bold','underline'))
+						title_label.place(rely=0.06,relx=0.5,anchor=CENTER)
+
+						contestant1_score_label = tkinter.Label(completeFrame, text="", font=('Tahoma', 10, 'bold'), fg='black', bg='white')
+						contestant1_score_label.place(rely=0.3, relx=0.35, anchor='center')
+
+						c.execute("SELECT * From member WHERE username=?", (memberUsername,))
+						member1 = c.fetchone()
+						if not member1:
+							messagebox.showinfo("info", "There is no username named " + memberUsername)
+							completeFrame.withdraw()
+
+						else:
+
+							contestant1_score_label.config(text= member1[2] + " " + member1[3] + ":")
+
+						contestant2_score_label = tkinter.Label(completeFrame, text="", font=('Tahoma', 10, 'bold'), fg='black', bg='white')
+						contestant2_score_label.place(rely=0.5, relx=0.35, anchor='center')
+
+						c.execute("SELECT * From member WHERE username=?", (memberUsername2,))
+						member2 = c.fetchone()
+						if not member2:
+							messagebox.showinfo("info", "There is no username named " + memberUsername2)
+							completeFrame.withdraw()
+
+						else:
+
+							contestant2_score_label.config(text= member2[2] + " " + member2[3] + ":")
+
+						contestant1_score_entry = tkinter.Entry(completeFrame, width=4, textvariable=contestant1Score, bd=4, relief='ridge', cursor="tcross", font=('Tahoma', 10, 'bold'))
+						contestant1_score_entry.place(rely=0.3, relx=0.8, anchor='center')
+						contestant1Score.set('')
+
+						contestant2_score_entry = tkinter.Entry(completeFrame, width=4, textvariable=contestant2Score, bd=4, relief='ridge', cursor="tcross", font=('Tahoma', 10, 'bold'))
+						contestant2_score_entry.place(rely=0.5, relx=0.8, anchor='center')
+						contestant2Score.set('')
+
+						confirm_scores_button=Button(completeFrame,text = 'Confirm Scores', command = lambda : finalCompetitionCompletition(completeFrame, contestant1_score_label, contestant2_score_label, contestant1_score_label, contestant2_score_label), fg ='white', bg='black', relief= 'groove', font = ('Verdana',11,'bold'), padx =35)
+						confirm_scores_button.place(rely=0.93,relx=0.5,anchor=CENTER)
+
+
+					returnColour(username_label, username2_label, start_date_label, end_date_label, view_group_label)
+
+			conn.commit()
+			conn.close()
+
+
+		def finalCompetitionCompletition(frame, username1, username2, label1, label2):
+			conn = sqlite3.connect('BadmintonClub.db')
+			c = conn.cursor()
+
+			frame.withdraw()
+
 			isValid = True
-			isValid = isValid and validate_group_selected(self.memberGroups.get(), "Member Group Selected", view_group_label)
+			isValid = isValid and validate_member_scores(contestant1Score.get(), contestant2Score.get(), "Member Score 1", "Member Score 2", label1, label2)
 
 			if isValid:
-				member_group_selected = self.memberGroups.get()
+				final_member1_score = contestant1Score.get()
+				final_member2_score = contestant2Score.get()
 
-				c.execute("SELECT * From competition WHERE competitionID=?", (member_group_selected,))
+				final_score = final_member1_score + "-" + final_member2_score
+
+				memberID = self.memberGroups.get()
+
+				c.execute("SELECT * From competition WHERE competitionID=?", (memberID,))
 				items = c.fetchone()
 				if not items:
-					messagebox.showinfo("info", "There is no group that exists with that number")
+					messagebox.showinfo("info", "There is no group that exists with that number", icon="error")
 
 				else:
 
 					memberUsername = items[0]
 					memberUsername2 = items[1]
 
+					c.execute("""UPDATE competition SET score = :score WHERE competitionID=:competitionID""", {
+						"score": str(final_score),
+						"competitionID": memberID
+					})
 
-					c.execute("SELECT * From member WHERE username=?", (memberUsername,))
-					member1 = c.fetchone()
-					if not member1:
-						messagebox.showinfo("info", "There is no username named " + memberUsername)
+					member_winner = ""
 
-					else:
+					if int(final_member1_score) == 21:
+						c.execute("SELECT * From member WHERE username=?", (memberUsername,))
+						member1 = c.fetchone()
+						if not member1:
+							messagebox.showinfo("info", "There is no username named " + memberUsername)
 
-						contestant1_label.config(text= member1[2] + " " + member1[3])
+						else:
 
-					c.execute("SELECT * From member WHERE username=?", (memberUsername2,))
-					member2 = c.fetchone()
-					if not member2:
-						messagebox.showinfo("info", "There is no username named " + memberUsername2)
+							member_winner = member1[2] + " " + member1[3]
 
-					else:
+					if int(final_member2_score) == 21:
+						c.execute("SELECT * From member WHERE username=?", (memberUsername2,))
+						member2 = c.fetchone()
+						if not member2:
+							messagebox.showinfo("info", "There is no username named " + memberUsername2)
 
-						contestant2_label.config(text= member2[2] + " " + member2[3])
+						else:
 
-				returnColour(username_label, username2_label, start_date_label, end_date_label, view_group_label)
-				messagebox.showinfo("info", "The competition has now been populated")
+							member_winner = member2[2] + " " + member2[3]
+
+
+
+					c.execute("""UPDATE competition SET winner = :winner WHERE competitionID=:competitionID""", {
+						"winner": str(member_winner),
+						"competitionID": memberID
+					})
+
+					messagebox.showinfo("info", "The final score has been saved as " + str(final_score))
+					messagebox.showinfo("info", "The winner is " + str(member_winner))
+
 
 			conn.commit()
 			conn.close()
+
+			treeviewPopulate()
 
 
 
 		startDate=StringVar()
 		endDate=StringVar()
+
+		contestant1Score = StringVar()
+		contestant2Score = StringVar()
 
 
 
@@ -427,7 +568,7 @@ class ResultsContent:
 		confirm_group_button = tkinter.Button(self.results, cursor="tcross",text="Confirm", command=confirmCompetition, fg='white', bg='black', bd=4, relief='ridge', font=('Tahoma', 10, 'bold'), padx=40)
 		confirm_group_button.place(rely=0.163, relx=0.86, anchor='center')
 
-		complete_competition_button = tkinter.Button(self.results, cursor="tcross",text="Complete Competition", command=completeCompetition, fg='white', bg='black', bd=4, relief='ridge', font=('Tahoma', 10, 'bold'), padx=5, pady=8)
+		complete_competition_button = tkinter.Button(self.results, cursor="tcross",text="Complete Competition", command=lambda : completeCompetition(contestant1_label, contestant2_label), fg='white', bg='black', bd=4, relief='ridge', font=('Tahoma', 10, 'bold'), padx=5, pady=8)
 		complete_competition_button.place(rely=0.582, relx=0.658, anchor='center')
 
 
